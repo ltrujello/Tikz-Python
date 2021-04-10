00d2f9202a2691cf4722970467ba9cd657a811c3
ltrujello <ltrujillo@g.hmc.edu>
2021-04-09 17:15:00 -0700

    Added missing f to f-string

--- tikzpy/utils/helpers.py
+++ tikzpy/utils/helpers.py
@@ -1,4 +1,4 @@
-from pathlib import WindowsPath, PosixPath
+from pathlib import WindowsPath
 
 
 def brackets(string):
@@ -24,7 +24,7 @@ def true_posix_path(path_obj):
     full_path = path_obj.resolve()
     if isinstance(path_obj, WindowsPath):
         drive = full_path.drive  # C:, E:, etc.
-        return str(full_path.relative_to("{drive}/").as_posix())
+        return str(full_path.relative_to(f"{drive}/").as_posix())
     else:
         # Must be PosixPath (We use Path, and pathlib automatically uses either WindowsPath or PosixPath)
         return str(full_path.resolve())
